22b79ae1dc059a2740faca2413e6230026bfcb45
jdev <73403761+sw918@users.noreply.github.com>
2022-10-05 12:26:53 +0200

    Add difficulties

--- test.py
+++ test.py
@@ -1,16 +1,39 @@
 # Test 000 Python - Beginners
 # Guess the number game between 1 & 10
 # Author: jdev
+# Version 0.2
 
 from random import Random, randint, random
 
 # Variables
-a = randint(1, 10)
 num = 0
 cont = 0
 
-while int(num) != int(a):
-    num = input("Guess the number: ")
-    cont = cont + 1
+# Choosing difficulty
+print("Hello! Welcome to Guess the number v0.2")
+print("Please, choose a difficulty:")
+print("\t1. Easy (1 - 10)")
+print("\t2. Medium (1 - 100)")
+print("\t3. Hard (1 - 1000)")
+diff = input("> ")
+print()
 
-print(f"Good job! You guess the number in {cont} tries!")
+# Start the game
+if int(diff) == 1:
+    gnum = randint(1, 10)
+    while int(num) != int(gnum):
+        num = input("Guess the number: ")
+        cont += 1
+elif int(diff) == 2:
+    gnum = randint(1, 100)
+    while int(num) != int(gnum):
+        num = input("Guess the number: ")
+        cont += 1
+if int(diff) == 3:
+    gnum = randint(1, 1000)
+    while int(num) != int(gnum):
+        num = input("Guess the number: ")
+        cont += 1
+
+# Win!
+print(f"\nGood job! You guess the number in {cont} tries!")
